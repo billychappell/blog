043b75fed4c7d1abff3cca481c4e4f0ee9a6bd52
Billy Chappell <chappell28@gmail.com>
2015-09-01 04:52:31 -0600

    i know it's terrible, but can't remember

--- main.go
+++ main.go
@@ -16,32 +16,38 @@ var (
 )
 
 func main() {
-	// Configures http server.
 
+	// Initialize server and parse flags.
 	var s http.Server
-	flag.BoolVar(&http2.VerboseLogs, "verbose", false, "verbose HTTP/2 debugging.")
+	flag.BoolVar(&http2.VerboseLogs, "verbose", false, "verbose HTTP/2 debugging")
 	flag.Parse()
 	s.Addr = *addr
 
-	// Opens database connection and retrieves list of posts to store in memory
+	// Open database and retrieve posts.
 	p, err := database.GetPosts()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Registers handlers and logs URL
+	// and port, configured for HTTP/2.
 	registerHandlers(p)
-
 	url := "https://" + *addr + "/"
 	log.Printf("Listening on " + url)
 	http2.ConfigureServer(&s, &http2.Server{})
 
+	// Starts listening over regular HTTP
+	// if the "-http" flag is non-empty.
 	if *httpAddr != "" {
 		go func() { log.Fatal(http.ListenAndServe(*httpAddr, nil)) }()
 	}
 
+	// Concurrently begins listening at URL for
+	// tls encrypted HTTP/2 communications.
 	go func() {
 		log.Fatal(s.ListenAndServeTLS("out/dev.crt", "out/dev.key"))
 	}()
 
+	// Blocks forever.
 	select {}
 }
